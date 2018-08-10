56a1ac6400283fee85c8eb9aa9c97e83446c76ec
Bruno Martins <bgcngm@gmail.com>
2018-08-10 13:47:04 +0100

    gts28velte: Consider lineage_gts28velte product via the AOSP way
    Change-Id: Ibdeb62f0aa30933a09ac12ca825025e5f18574ab

--- lineage_gts28velte.mk
+++ lineage_gts28velte.mk
@@ -21,9 +21,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from gts28velte device
 $(call inherit-product, device/samsung/gts28velte/device.mk)
 
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gts28velte
-PRODUCT_NAME := full_gts28velte
+PRODUCT_NAME := lineage_gts28velte
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := gts28velte
+PRODUCT_MODEL := SM-T719
 PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+# Use the latest approved GMS identifiers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=gts28veltexx \
+    PRIVATE_BUILD_DESC="gts28veltexx-user 7.0 NRD90M T719XXU2BRB2 release-keys"
+
+BUILD_FINGERPRINT := "samsung/gts28veltexx/gts28velte:7.0/NRD90M/T719XXU2BRB2:user/release-keys"
